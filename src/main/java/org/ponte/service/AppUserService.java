e59568d8b05c7bda6622164f22a7b8cd96f74aa5
Peter_Szabo <petersz.andrassy@gmail.com>
2024-03-19 10:51:05 +0100

    done appUser service

---
+++ src/main/java/org/ponte/service/AppUserService.java
@@ -0,0 +1,19 @@
+package org.ponte.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class AppUserService {
+
+    private final ModelMapper modelMapper;
+    private final AppUserRepository appUserRepository;
+
+    public AppUserService(ModelMapper modelMapper, AppUserRepository appUserRepository) {
+        this.modelMapper = modelMapper;
+        this.appUserRepository = appUserRepository;
+    }
+}
